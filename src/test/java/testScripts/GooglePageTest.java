278eb04d2c6740e92361bec3dbb1a9661dd6aa4f
mohitkurmi1315 <emailid>
2024-04-25 07:48:54 +0000

    testNg commit

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,20 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GooglePageTest {
+  @Test
+  public void javaSearchTest() {
+	    WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+	    WebElement srcbox = driver.findElement(By.id("APjFqb"));
+	    srcbox.sendKeys("Java tutorial");
+	    srcbox.sendKeys(Keys.ENTER);
+  }
+}
